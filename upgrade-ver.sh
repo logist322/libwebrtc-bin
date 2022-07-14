9972ea38b185f4b43ffb8f8a631339fbe2e31d55
logist322 <hytornoy-99@mail.ru>
2022-07-14 20:37:27 +0400

    WIP

---
+++ upgrade-ver.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+curl 'https://chromiumdash.appspot.com/fetch_releases?channel=Stable&platform=Linux&num=1&offset=0' |
+sed 's/,/\n/g' | 
+sed 's/["|\}|\{|\[]//g' |
+sed 's/]//g' |
+grep '^version\|^webrtc' |
+while read p; do
+    name=$(echo "$p" | cut -d':' -f1)
+    value=$(echo "$p" | cut -d':' -f2)
+    field=$([[ $name = version ]] && echo "WEBRTC_VERSION" || echo "WEBRTC_COMMIT")
+    grep -n $field ./VERSION |
+    cut -d':' -f1 |
+    {
+        read line;
+        sed -i "${line}s|.*|${field}=${value}|g" ./VERSION;
+    }
+done
