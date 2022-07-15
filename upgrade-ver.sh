a37ffcc4cfa555d6e45db773c1758da0bd745e69
logist322 <hytornoy-99@mail.ru>
2022-07-15 19:50:00 +0400

    WIP

--- upgrade-ver.sh
+++ upgrade-ver.sh
@@ -1,26 +1,22 @@
 #!/bin/bash
 
-curl 'https://chromiumdash.appspot.com/fetch_releases?channel=Stable&platform=Linux&num=1&offset=0' |
-{
-    read res
-    if [[ "$res" != *"\"versiown\""* ]]; then
-        echo "There is wrong \`Chrome API\`, no \`version\` field there."
-        exit 1
-    fi
-
-    if [[ "$res" != *"\"webrtc\""* ]]; then
-        echo "There is wrong \`Chrome API\`, no \`webrtc\` field there."
-        exit 1
-    fi
-    echo "$res"
-} |
+res=$(curl 'https://chromiumdash.appspot.com/fetch_releases?channel=Stable&platform=Linux&num=1&offset=0')
+if [[ "$res" != *"\"verssion\""* ]]; then
+    echo "There is wrong \`Chrome API\`, no \`version\` field there."
+    exit 1
+fi
+if [[ "$res" != *"\"webrtc\""* ]]; then
+    echo "There is wrong \`Chrome API\`, no \`webrtc\` field there."
+    exit 1
+fi
+echo "$res" |
 sed 's/,/\n/g' |
 sed 's/["|\}|\{|\[]//g' |
 sed 's/]//g' |
 grep '^version\|^webrtc' |
-while read p; do
-    name=$(echo "$p" | cut -d':' -f1)
-    value=$(echo "$p" | cut -d':' -f2)
+while read data; do
+    name=$(echo "$data" | cut -d':' -f1)
+    value=$(echo "$data" | cut -d':' -f2)
     field=$([[ $name = version ]] && echo "WEBRTC_VERSION" || echo "WEBRTC_COMMIT")
     grep -n $field ./VERSION |
     cut -d':' -f1 |
