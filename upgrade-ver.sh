16504e216be3232a102f3b4cbb5c2e4a61bc0532
logist322 <hytornoy-99@mail.ru>
2022-07-15 19:42:44 +0400

    WIP

--- upgrade-ver.sh
+++ upgrade-ver.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
 
 curl 'https://chromiumdash.appspot.com/fetch_releases?channel=Stable&platform=Linux&num=1&offset=0' |
-sed 's/,/\n/g' | 
+{
+    read res
+    if [[ "$res" != *"\"versiown\""* ]]; then
+        echo "There is wrong \`Chrome API\`, no \`version\` field there."
+        exit 1
+    fi
+
+    if [[ "$res" != *"\"webrtc\""* ]]; then
+        echo "There is wrong \`Chrome API\`, no \`webrtc\` field there."
+        exit 1
+    fi
+    echo "$res"
+} |
+sed 's/,/\n/g' |
 sed 's/["|\}|\{|\[]//g' |
 sed 's/]//g' |
 grep '^version\|^webrtc' |
@@ -12,11 +25,10 @@ while read p; do
     grep -n $field ./VERSION |
     cut -d':' -f1 |
     {
-        read line;
-        sed -i "${line}s|.*|${field}=${value}|g" ./VERSION;
+        read line
+        sed -i "${line}s|.*|${field}=${value}|g" ./VERSION
     }
-    if [ $name = version ]
-    then
+    if [ $name = version ]; then
         echo "::set-output name=$(echo $name)::$(echo $value)"
     fi
 done
